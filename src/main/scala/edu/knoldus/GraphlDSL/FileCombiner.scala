0229ae41381babf2c79ce22ff5b80211dd060454
akashkr17 <akash.kumar@knoldus.com>
2022-05-05 15:53:30 +0530

    update the graph

--- src/main/scala/edu/knoldus/GraphlDSL/FileCombiner.scala
+++ src/main/scala/edu/knoldus/GraphlDSL/FileCombiner.scala
@@ -1,106 +1,106 @@
-package edu.knoldus.GraphlDSL
-
-import java.nio.file.{Path, Paths}
-
-import akka.NotUsed
-import akka.stream.FlowShape
-import akka.stream.alpakka.file.scaladsl.Directory
-import akka.stream.scaladsl.{FileIO, Flow, GraphDSL, Merge, Partition}
-import io.circe._
-import io.circe.generic.auto._
-import io.circe.parser._
-import io.circe.syntax._
-import spray.json.{DefaultJsonProtocol, RootJsonFormat}
-
-import spray.json._
-object FileCombiner extends App with DefaultJsonProtocol {
-  sealed trait ProcessableFile
-  case class Document1(metadata: Metadata, text: Text)
-  case class Metadata(id: String) extends ProcessableFile
-  case class Text(text: String) extends ProcessableFile
-
-
-  implicit val metadata: RootJsonFormat[Metadata] = jsonFormat1(Metadata)
-  implicit val text: RootJsonFormat[Text] = jsonFormat1(Text)
-  implicit val document: RootJsonFormat[Document1] = jsonFormat2(Document1)
-
-
-  val directory = "/documents/"
-
-  val files = Directory
-    .ls(Paths.get(directory))
-    .flatMapConcat(Directory.ls)
-
-  def constructDocumentFromPaths: Flow[Path, Document, NotUsed] = {
-    Flow.fromGraph(GraphDSL.create() { implicit builder =>
-      import GraphDSL.Implicits._
-
-      val filesSplitter = builder.add(Partition[Path](3, partitionFiles))
-      val merge = builder.add(Merge[Option[ProcessableFile]](3))
-
-      // Process metadata files
-      filesSplitter
-        .out(1) ~> extractFileContentFromPath ~> converter ~> merge.in(0)
-
-      // Process text files
-      filesSplitter
-        .out(1) ~> extractFileContentFromPath ~> parseJson[Text].map(Some(_)) ~> merge.in(1)
-
-      // Simply ignore and merge in any file that isn't a text extraction result
-      filesSplitter
-        .out(2)
-        .log("SkippingFile")
-        .map(_ => None) ~> merge.in(2)
-
-      val documents = merge.out
-        .scan(DocumentAccumulator(None, None))((document, result) => {
-          result match {
-            case Some(metadata: Metadata) =>
-              document.copy(metadata = Some(metadata))
-            case Some(text: Text) =>
-              document.copy(text = Some(text))
-            case _ =>
-              document
-          }
-        })
-        .filter(_.isComplete)
-        .map(Document.apply)
-
-      FlowShape(filesSplitter.in, documents.outlet)
-    })
-  }
-
-  private def converter: Flow[String, Metadata, NotUsed] = Flow[String].map {
-    data => data.parseJson.convertTo[Metadata]
-  }
-  private def extractFileContentFromPath: Flow[Path, String, NotUsed] =
-    Flow[Path]
-      .flatMapConcat(path => {
-        FileIO.fromPath(path).reduce((a, b) => a ++ b)
-      })
-      .map(_.utf8String)
-  case class DocumentAccumulator(metadata: Option[Metadata], text: Option[Text]) {
-    def isComplete: Boolean = metadata.isDefined && text.isDefined
-  }
-
-  case class Document(metadata: Metadata, text: Text)
-
-  object Document {
-    def apply(accumulator: DocumentAccumulator): Document = {
-      if (accumulator.metadata.isEmpty || accumulator.text.isEmpty) {
-        throw new RuntimeException("Unable to create Document from DocumentAccumulator with empty values")
-      }
-      new Document(accumulator.metadata.get, accumulator.text.get)
-    }
-  }
-
-  def partitionFiles(path: Path) = {
-    path.getFileName.toString match {
-      case "metadata.json" => 0
-      case "text.json"     => 1
-      case _               => 2
-    }
-  }
-
-
-}
+//package edu.knoldus.GraphlDSL
+//
+//import java.nio.file.{Path, Paths}
+//
+//import akka.NotUsed
+//import akka.stream.FlowShape
+//import akka.stream.alpakka.file.scaladsl.Directory
+//import akka.stream.scaladsl.{FileIO, Flow, GraphDSL, Merge, Partition}
+//import io.circe._
+//import io.circe.generic.auto._
+//import io.circe.parser._
+//import io.circe.syntax._
+//import spray.json.{DefaultJsonProtocol, RootJsonFormat}
+//
+//import spray.json._
+//object FileCombiner extends App with DefaultJsonProtocol {
+//  sealed trait ProcessableFile
+//  case class Document1(metadata: Metadata, text: Text)
+//  case class Metadata(id: String) extends ProcessableFile
+//  case class Text(text: String) extends ProcessableFile
+//
+//
+//  implicit val metadata: RootJsonFormat[Metadata] = jsonFormat1(Metadata)
+//  implicit val text: RootJsonFormat[Text] = jsonFormat1(Text)
+//  implicit val document: RootJsonFormat[Document1] = jsonFormat2(Document1)
+//
+//
+//  val directory = "/documents/"
+//
+//  val files = Directory
+//    .ls(Paths.get(directory))
+//    .flatMapConcat(Directory.ls)
+//
+//  def constructDocumentFromPaths: Flow[Path, Document, NotUsed] = {
+//    Flow.fromGraph(GraphDSL.create() { implicit builder =>
+//      import GraphDSL.Implicits._
+//
+//      val filesSplitter = builder.add(Partition[Path](3, partitionFiles))
+//      val merge = builder.add(Merge[Option[ProcessableFile]](3))
+//
+//      // Process metadata files
+//      filesSplitter
+//        .out(1) ~> extractFileContentFromPath ~> converter ~> merge.in(0)
+//
+//      // Process text files
+//      filesSplitter
+//        .out(1) ~> extractFileContentFromPath ~> parseJson[Text].map(Some(_)) ~> merge.in(1)
+//
+//      // Simply ignore and merge in any file that isn't a text extraction result
+//      filesSplitter
+//        .out(2)
+//        .log("SkippingFile")
+//        .map(_ => None) ~> merge.in(2)
+//
+//      val documents = merge.out
+//        .scan(DocumentAccumulator(None, None))((document, result) => {
+//          result match {
+//            case Some(metadata: Metadata) =>
+//              document.copy(metadata = Some(metadata))
+//            case Some(text: Text) =>
+//              document.copy(text = Some(text))
+//            case _ =>
+//              document
+//          }
+//        })
+//        .filter(_.isComplete)
+//        .map(Document.apply)
+//
+//      FlowShape(filesSplitter.in, documents.outlet)
+//    })
+//  }
+//
+//  private def converter: Flow[String, Metadata, NotUsed] = Flow[String].map {
+//    data => data.parseJson.convertTo[Metadata]
+//  }
+//  private def extractFileContentFromPath: Flow[Path, String, NotUsed] =
+//    Flow[Path]
+//      .flatMapConcat(path => {
+//        FileIO.fromPath(path).reduce((a, b) => a ++ b)
+//      })
+//      .map(_.utf8String)
+//  case class DocumentAccumulator(metadata: Option[Metadata], text: Option[Text]) {
+//    def isComplete: Boolean = metadata.isDefined && text.isDefined
+//  }
+//
+//  case class Document(metadata: Metadata, text: Text)
+//
+//  object Document {
+//    def apply(accumulator: DocumentAccumulator): Document = {
+//      if (accumulator.metadata.isEmpty || accumulator.text.isEmpty) {
+//        throw new RuntimeException("Unable to create Document from DocumentAccumulator with empty values")
+//      }
+//      new Document(accumulator.metadata.get, accumulator.text.get)
+//    }
+//  }
+//
+//  def partitionFiles(path: Path) = {
+//    path.getFileName.toString match {
+//      case "metadata.json" => 0
+//      case "text.json"     => 1
+//      case _               => 2
+//    }
+//  }
+//
+//
+//}
--- src/main/scala/edu/knoldus/backpressure/BackpressureMain.scala
+++ src/main/scala/edu/knoldus/backpressure/BackpressureMain.scala
@@ -26,45 +26,45 @@ object BackpressureMain extends App {
     println(s"$x")
   }
 
-  val graph1 = input.via(flow).async.to(output)
-  val graph2 = input.via(flow.buffer(10,OverflowStrategy.backpressure)).async.to(output)
-
-  val graph3 = input.via(flow.buffer(10,OverflowStrategy.dropHead)).async.to(output)
-  val graph4 = input.via(flow.buffer(10,OverflowStrategy.dropTail)).async.to(output)
-  val graph5 = input.via(flow.buffer(10,OverflowStrategy.dropNew)).async.to(output).run
+  val graph1 = input.via(flow).async.to(output).run
+//  val graph2 = input.via(flow.buffer(10,OverflowStrategy.backpressure)).async.to(output)
+//
+//  val graph3 = input.via(flow.buffer(10,OverflowStrategy.dropHead)).async.to(output)
+//  val graph4 = input.via(flow.buffer(10,OverflowStrategy.dropTail)).async.to(output)
+//  val graph5 = input.via(flow.buffer(10,OverflowStrategy.dropNew)).async.to(output).run
 
 }
 
-object StreamApp  {
-  def lineSink(filename: String): Sink[String, Future[IOResult]] = {
-    Flow[String]
-      .alsoTo(Sink.foreach(s => println(s"$filename: $s")))
-      .map(s => ByteString(s + "\n"))
-      .toMat(FileIO.toFile(new File(filename)))(Keep.right)
-  }
-  def main(args: Array[String]): Unit = {
-    implicit val system = ActorSystem("streamapp")
-    implicit val materializer = ActorMaterializer()
-    val source: Source[Int, NotUsed] = Source(1 to 100)
-    val factorials: Source[BigInt, NotUsed] =
-      source.scan(BigInt(1))((acc, next) => acc * next)
-    val sink1 = lineSink("factorial1.txt")
-    val sink2 = lineSink("factorial2.txt")
-    val slowSink2 = Flow[String].via(Flow[String]
-      .throttle(1, 1.second, 1, ThrottleMode.shaping))
-      .toMat(sink2)(Keep.right)
-    val bufferedSink2 = Flow[String].buffer(50, OverflowStrategy.dropNew)
-      .via(Flow[String]
-        .throttle(1, 1.second, 1, ThrottleMode.shaping))
-      .toMat(sink2)(Keep.right)
-    val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
-      import GraphDSL.Implicits._
-      val bcast = b.add(Broadcast[String](2))
-      factorials.map(_.toString) ~> bcast.in
-      bcast.out(0) ~> sink1
-      bcast.out(1) ~> sink2
-      ClosedShape
-    })
-    g.run()
-  }
-}
+//object StreamApp  {
+//  def lineSink(filename: String): Sink[String, Future[IOResult]] = {
+//    Flow[String]
+//      .alsoTo(Sink.foreach(s => println(s"$filename: $s")))
+//      .map(s => ByteString(s + "\n"))
+//      .toMat(FileIO.toFile(new File(filename)))(Keep.right)
+//  }
+//  def main(args: Array[String]): Unit = {
+//    implicit val system = ActorSystem("streamapp")
+//    implicit val materializer = ActorMaterializer()
+//    val source: Source[Int, NotUsed] = Source(1 to 100)
+//    val factorials: Source[BigInt, NotUsed] =
+//      source.scan(BigInt(1))((acc, next) => acc * next)
+//    val sink1 = lineSink("factorial1.txt")
+//    val sink2 = lineSink("factorial2.txt")
+//    val slowSink2 = Flow[String].via(Flow[String]
+//      .throttle(1, 1.second, 1, ThrottleMode.shaping))
+//      .toMat(sink2)(Keep.right)
+//    val bufferedSink2 = Flow[String].buffer(50, OverflowStrategy.dropNew)
+//      .via(Flow[String]
+//        .throttle(1, 1.second, 1, ThrottleMode.shaping))
+//      .toMat(sink2)(Keep.right)
+//    val g = RunnableGraph.fromGraph(GraphDSL.create() { implicit b =>
+//      import GraphDSL.Implicits._
+//      val bcast = b.add(Broadcast[String](2))
+//      factorials.map(_.toString) ~> bcast.in
+//      bcast.out(0) ~> sink1
+//      bcast.out(1) ~> sink2
+//      ClosedShape
+//    })
+//    g.run()
+//  }
+//}
